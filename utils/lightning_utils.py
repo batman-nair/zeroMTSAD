1ee2a7a516e1aa8630b6fb36659b0a0d2404e9ad
Arjun Nair <arjunnair.mec@gmail.com>
2024-05-26 02:42:35 +0200

    Fix config dump for test only runs

--- utils/lightning_utils.py
+++ utils/lightning_utils.py
@@ -7,7 +7,15 @@ class SaveConfigCallback(Callback):
     def __init__(self, config: dict):
         super().__init__()
         self.config = config
+        self.saved = False
 
     def on_train_start(self, trainer, pl_module):
+        self.saved = True
+        with open(os.path.join(trainer.logger.log_dir, 'config.json'), 'w') as ff:
+            json.dump(self.config, ff)
+
+    def on_test_start(self, trainer, pl_module):
+        if self.saved:
+            return
         with open(os.path.join(trainer.logger.log_dir, 'config.json'), 'w') as ff:
             json.dump(self.config, ff)
