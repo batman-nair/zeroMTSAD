7a1ab5dab145ccbc3ceeba3701b8031d7eeb51a3
Arjun Nair <arjunnair.mec@gmail.com>
2024-06-25 01:39:49 +0200

    Use zero shot validation datasets

--- datasets/smd.py
+++ datasets/smd.py
@@ -9,7 +9,8 @@ class SMDDataModule(lp.LightningDataModule):
     SMD_NUM_FEATURES = 38
     def __init__(self, data_params: dict, train_pipeline: dict={}, test_pipeline: dict={}):
         super().__init__()
-        self.training_server_ids = data_params['training_server_ids']
+        self.train_server_ids = data_params['train_server_ids']
+        self.val_server_ids = data_params['val_server_ids']
         self.test_server_ids = data_params['test_server_ids']
         self.train_pipeline = train_pipeline
         self.test_pipeline = test_pipeline
@@ -21,15 +22,15 @@ class SMDDataModule(lp.LightningDataModule):
 
     def setup(self, stage: str) -> None:
         if stage == 'fit':
-            self.smd_datasets = [SMDDataset(server_id=server_id, training=True) for server_id in self.training_server_ids]
-            ds_splits = [list(make_dataset_split(ds, *self.data_splits, axis='time')) for ds in self.smd_datasets]
-            transformed_train_data = [make_pipe_from_dict(self.train_pipeline, ds_split[0]) for ds_split in ds_splits]
-            transformed_val_data = [make_pipe_from_dict(self.train_pipeline, ds_split[1]) for ds_split in ds_splits]
+            self.train_datasets = [SMDDataset(server_id=server_id, training=True) for server_id in self.train_server_ids]
+            self.val_datasets = [SMDDataset(server_id=server_id, training=True) for server_id in self.val_server_ids]
+            transformed_train_data = [make_pipe_from_dict(self.train_pipeline, DatasetSource(train_dataset)) for train_dataset in self.train_datasets]
+            transformed_val_data = [make_pipe_from_dict(self.train_pipeline, DatasetSource(val_dataset)) for val_dataset in self.val_datasets]
             self.combined_train_dataset = torch.utils.data.ConcatDataset(transformed_train_data)
             self.combined_val_dataset = torch.utils.data.ConcatDataset(transformed_val_data)
         elif stage == 'test':
-            self.smd_datasets = [SMDDataset(server_id=server_id, training=False) for server_id in self.test_server_ids]
-            transfomed_test_data = [make_pipe_from_dict(self.test_pipeline, DatasetSource(ds)) for ds in self.smd_datasets]
+            self.test_datasets = [SMDDataset(server_id=server_id, training=False) for server_id in self.test_server_ids]
+            transfomed_test_data = [make_pipe_from_dict(self.test_pipeline, DatasetSource(ds)) for ds in self.test_datasets]
             self.combined_test_dataset = torch.utils.data.ConcatDataset(transfomed_test_data)
 
     def train_dataloader(self) -> torch.utils.data.DataLoader:
