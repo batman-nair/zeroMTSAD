79d2316bf2fa0c7b8ff5d68ed85f4290910f28cc
Arjun Nair <arjunnair.mec@gmail.com>
2024-09-23 18:26:07 +0200

    Add GRU GMM VAE model

---
+++ experiments/gru_gmm_vae.py
@@ -0,0 +1,21 @@
+from experiments.timesead_wrapper import LitTimeSeADModel
+from timesead.models.generative import GRUGMMVAE, GMMVAELoss, GMMVAEAnomalyDetector
+from timesead_experiments.generative.vae.train_gmm_vae import get_training_pipeline, get_test_pipeline
+
+import torch
+
+class LitGRUGMMVAE(LitTimeSeADModel):
+    def __init__(self, seq_len, num_features, model_params, run_params):
+        super().__init__(run_params)
+        self.model = GRUGMMVAE(num_features, **model_params)
+        self.loss = GMMVAELoss()
+        self.save_hyperparameters(model_params)
+
+    def setup_detector(self, detector_params: dict, val_loader: torch.utils.data.DataLoader) -> None:
+        self.detector = GMMVAEAnomalyDetector(self.model, **detector_params)
+
+
+MODEL = LitGRUGMMVAE
+TRAIN_PIPELINE = get_training_pipeline()
+TEST_PIPELINE = get_test_pipeline()
+
