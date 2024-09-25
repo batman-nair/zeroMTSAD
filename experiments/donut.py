923543db64ed6b7d0ad3bb5c83b5b594f6bfb5d0
Arjun Nair <arjunnair.mec@gmail.com>
2024-09-25 18:51:24 +0200

    Add Donut model

---
+++ experiments/donut.py
@@ -0,0 +1,20 @@
+from experiments.timesead_wrapper import LitTimeSeADModel
+from timesead.models.generative import Donut, DonutAnomalyDetector, MaskedVAELoss
+from timesead_experiments.generative.vae.train_donut import get_training_pipeline, get_test_pipeline
+
+import torch
+
+class LitDonut(LitTimeSeADModel):
+    def __init__(self, seq_len, num_features, model_params, run_params):
+        super().__init__(run_params)
+        self.model = Donut(num_features * seq_len, **model_params)
+        self.loss = MaskedVAELoss()
+        self.save_hyperparameters(model_params)
+
+    def setup_detector(self, detector_params: dict, val_loader: torch.utils.data.DataLoader) -> None:
+        self.detector = DonutAnomalyDetector(self.model, **detector_params)
+
+
+MODEL = LitDonut
+TRAIN_PIPELINE = get_training_pipeline()
+TEST_PIPELINE = get_test_pipeline()
