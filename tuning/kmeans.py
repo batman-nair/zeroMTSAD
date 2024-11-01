b25cf1acb1bda0593e9d89a82e9869e3777b1cf6
Arjun Nair <arjunnair.mec@gmail.com>
2024-11-01 12:35:56 +0100

    Add tuning for kmeans

--- tuning/kmeans.py
+++ tuning/kmeans.py
@@ -7,6 +7,7 @@ def generate_trial_overrides(trial: optuna.trial.Trial) -> List[str]:
     k = trial.suggest_int('k', 1, 101, step=5)
     batch_size = trial.suggest_int('batch_size', 512, 10240, step=512)
     window_size = trial.suggest_int('window_size', 5, 100, step=5)
+    step_size = trial.suggest_int('step_size', 1, 10, step=1)
 
     overrides = [
         f'batch_size={batch_size}',
@@ -15,5 +16,7 @@ def generate_trial_overrides(trial: optuna.trial.Trial) -> List[str]:
         f'transforms.train.window.args.window_size={window_size}',
         f'transforms.test.window.args.window_size={window_size}',
         f'transforms.seq_len={window_size}',
+        f'transforms.train.window.args.step_size={step_size}',
+        f'transforms.test.window.args.step_size={step_size}',
     ]
     return overrides
