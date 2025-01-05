2f60b5c74821f9487a49ae6154a82692fd21b0ac
Arjun Nair <arjunnair.mec@gmail.com>
2025-01-05 12:26:32 +0100

    Update plot script to work with new configs and multiple log files

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import json
 import importlib
 import torch
@@ -10,14 +11,20 @@ import timesead
 
 from utils.utils import recursive_update
 
+def _update_config_if_needed(config: dict):
+    data_params = config['data_params']
+    if 'train_server_ids' not in data_params:
+        return
+    new_data_params = {}
+    for key, val in data_params.items():
+        if 'server_ids' in key:
+            new_data_params[key.replace('server_ids', 'ids')] = val
+        else:
+            new_data_params[key] = val
+    new_data_params['num_workers'] = 4
+    config['data_params'] = new_data_params
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--log_dir', help='Path to the logging directory. Defaults to the directory of the checkpoint')
-    parser.add_argument('-t', '--plot_threshold', action='store_true', help='Plot threshold line for anomaly')
-    args = parser.parse_args()
-
-    log_dir = args.log_dir
+def generate_plots_for_log(log_dir: str, plot_threshold: bool):
     model_path = os.path.join(log_dir, 'final_model.pth')
     if not os.path.exists(model_path):
         with open(os.path.join(log_dir, 'run.json'), 'r') as ff:
@@ -29,6 +36,8 @@ if __name__ == '__main__':
         config = json.load(ff)
     with open(results_path, 'r') as ff:
         results = json.load(ff)
+
+    _update_config_if_needed(config)
 
     data_import = importlib.import_module(f'datasets.{config["dataset"]}')
     experiment_import = importlib.import_module(f'experiments.{config["experiment"]}')
@@ -62,7 +71,7 @@ if __name__ == '__main__':
 
     print('Plotting anomalies')
     timesead.utils.plot_utils.plot_sequence_against_anomaly(scores, labels, scatter=False)
-    if args.plot_threshold:
+    if plot_threshold:
         plt.axhline(y=results['best_ts_f1_score']['other_info']['threshold'], color='dimgray', linestyle='--')
     plt.savefig(os.path.join(plot_dir, f'full.png'))
     plt.close()
@@ -81,7 +90,26 @@ if __name__ == '__main__':
             scatter=False,
             xticks=[0, len_window//2, 3*len_window//2, 2*len_window]
             )
-        if args.plot_threshold:
+        if plot_threshold:
             plt.axhline(y=results['best_ts_f1_score']['other_info']['threshold'], color='dimgray', linestyle='--')
         plt.savefig(os.path.join(plot_dir, f'anomaly_{index}.png'))
         plt.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--log_dirs', help='Glob path to log directories', required=True)
+    parser.add_argument('-t', '--plot_threshold', action='store_true', help='Plot threshold line for anomaly')
+    parser.add_argument('--overwrite', action='store_true', help='Overwrite existing plots')
+    args = parser.parse_args()
+
+    for log_dir in glob.glob(args.log_dirs):
+        if os.path.exists(os.path.join(log_dir, 'plots')) and not args.overwrite:
+            print('Plots already exist for', log_dir)
+            continue
+        print('Generating plots for', log_dir)
+        try:
+            generate_plots_for_log(log_dir, args.plot_threshold)
+        except Exception as e:
+            print('Error generating plots for', log_dir, e)
+            continue
